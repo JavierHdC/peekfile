b84e2995e28f9cd7c5579f9656fcfd0310e785bb
JavierHdC <101563525+JavierHdC@users.noreply.github.com>
2023-10-23 23:24:06 +0200

    first change

--- peek.sh
+++ peek.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <input_file>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_file> <lines_to_display>"
     exit 1
 fi
 
 input_file="$1"
+lines_to_display="$2"
 
 if [ ! -f "$input_file" ]; then
     echo "File not found: $input_file"
     exit 1
 fi
 
+# Check if lines_to_display is a positive integer
+if ! [[ "$lines_to_display" =~ ^[0-9]+$ ]]; then
+    echo "Invalid value for lines_to_display: $lines_to_display (must be a positive integer)"
+    exit 1
+fi
+
 # Count the total number of lines in the file
 line_count=$(wc -l < "$input_file")
 
-if [ "$line_count" -le 6 ]; then
-    # If the file has 6 or fewer lines, just print the whole file
+if [ "$line_count" -le "$((2 * lines_to_display))" ]; then
+    # If the file has fewer lines than requested, print the whole file
     cat "$input_file"
 else
-    # Print the first 3 lines
-    head -n 3 "$input_file"
+    # Print the first N lines
+    head -n "$lines_to_display" "$input_file"
     # Print the ellipsis line
     echo "..."
-    # Print the last 3 lines
-    tail -n 3 "$input_file"
+    # Print the last N lines
+    tail -n "$lines_to_display" "$input_file"
 fi
+
